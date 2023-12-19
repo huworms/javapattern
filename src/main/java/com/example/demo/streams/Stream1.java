ef347a264ed265464101e29477ea3b9e140c2698
hugo_ <hugofi@gmail.com>
2023-12-19 17:52:23 -0600

    add Streams

---
+++ src/main/java/com/example/demo/streams/Stream1.java
@@ -0,0 +1,11 @@
+package com.example.demo.streams;
+
+public class Stream1 {
+ 
+ 
+ public static void main(String arg[]) {
+	 var a=new String("abc");
+	 System.out.println("El valor de a: "+a.toUpperCase());
+ }
+ 
+}
